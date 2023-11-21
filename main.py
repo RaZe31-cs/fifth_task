e9d8008abd3aba50b9afb9185db0600a4508ccc9
RaZe31-cs <gri.dubrovskih@gmail.com>
2023-11-21 22:44:40 +0500

    second_commit

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import sqlite3
 import sys
 
 from PyQt5 import uic
+from PyQt5.QtCore import QTimer
 from PyQt5.QtWidgets import QWidget, QApplication, QTableWidgetItem, QMainWindow
 
 
-class AddEditDatabase(QMainWindow):
+class AddEditDatabase(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi('addEditCoffeeForm.ui', self)
@@ -21,49 +22,95 @@ class AddEditDatabase(QMainWindow):
         self.table.setRowCount(len(res))
         # print(res)
         for i, info in enumerate(res):
-            self.table.setItem(i, 0, QTableWidgetItem(str(info[0])))
-            self.table.setItem(i, 1, QTableWidgetItem(str(info[1])))
-            self.table.setItem(i, 2, QTableWidgetItem(str(info[2])))
-            self.table.setItem(i, 3, QTableWidgetItem(str(info[3])))
-            self.table.setItem(i, 4, QTableWidgetItem(str(info[4])))
-            self.table.setItem(i, 5, QTableWidgetItem(str(info[5])))
-            self.table.setItem(i, 6, QTableWidgetItem(str(info[6])))
+            for j in range(7):
+                self.table.setItem(i, j, QTableWidgetItem(str(info[j])))
+        self.table.cellClicked.connect(self.push_table)
+        self.pushButton.clicked.connect(self.push_table)
+
+    def push_table(self, *args):
+        self.AddEdit = AddEditSmallForm(args, self)
+        self.AddEdit.show()
 
 
-class Main(QWidget):
-    def __init__(self):
+class AddEditSmallForm(QMainWindow):
+    def __init__(self, args, parent):
         super().__init__()
-        uic.loadUi('main.ui', self)
+        uic.loadUi('AddEdit.ui', self)
+        self.parent = parent
+        self.args = args
+        self.pushButton_2.clicked.connect(self.close_Form)
+        self.pushButton.clicked.connect(self.AddEdit)
         self.initUi()
 
     def initUi(self):
-        self.pushButton.clicked.connect(self.push_btn)
-        self.table.setColumnCount(7)
-        self.table.setHorizontalHeaderLabels(['ID', 'название сорт', 'степень обжарки', 'молотый/в зернах',
-                                              'описание вкуса', 'цена', 'объем упаковки'])
-        self.con = sqlite3.connect('coffee.sqlite')
-        self.cur = self.con.cursor()
-        res = self.cur.execute('SELECT * FROM coffee_info').fetchall()
-        self.table.setRowCount(len(res))
-        # print(res)
-        for i, info in enumerate(res):
-            self.table.setItem(i, 0, QTableWidgetItem(str(info[0])))
-            self.table.setItem(i, 1, QTableWidgetItem(str(info[1])))
-            self.table.setItem(i, 2, QTableWidgetItem(str(info[2])))
-            self.table.setItem(i, 3, QTableWidgetItem(str(info[3])))
-            self.table.setItem(i, 4, QTableWidgetItem(str(info[4])))
-            self.table.setItem(i, 5, QTableWidgetItem(str(info[5])))
-            self.table.setItem(i, 6, QTableWidgetItem(str(info[6])))
+        self.table_column = ('name_of_sort', 'degree_of_roasting',
+                 'ground_or_in_grains', 'info_taste', 'price', 'volume')
 
-    def push_btn(self):
-        FormAddEdit = AddEditDatabase()
-        FormAddEdit.show()
-        print(FormAddEdit)
-        print('Открыта форма добавления\редактирования')
+        self.comboBox.addItems(['Легкая', 'Средняя', 'Темная'])
+        self.comboBox_2.addItems(('Молотый', 'Зерна'))
+        if self.args[0] is False:
+            pass
+        else:
+            self.lineEdit.setText(self.parent.table.item(self.args[0], 1).text())
+
+            self.comboBox.setCurrentText(self.parent.table.item(self.args[0], 2).text())
+
+            self.comboBox_2.setCurrentText(self.parent.table.item(self.args[0], 3).text())
+
+            self.plainTextEdit.setPlainText(self.parent.table.item(self.args[0], 4).text())
+
+            self.spinBox.setValue(int(self.parent.table.item(self.args[0], 5).text()))
+
+            self.doubleSpinBox.setValue(float(self.parent.table.item(self.args[0], 6).text()))
+
+    def close_Form(self):
+        self.close()
+
+    def AddEdit(self):
+        if self.check_confirm_form():
+            if self.args[0] is False:
+                self.parent.cur.execute('''INSERT INTO coffee_info(name_of_sort, degree_of_roasting,
+                 ground_or_in_grains, info_taste, price, volume) VALUES(?, ?, ?, ?, ?, ?)''',
+                                        (self.lineEdit.text().strip(),
+                                         self.comboBox.currentText().strip(),
+                                         self.comboBox_2.currentText().strip(),
+                                         self.plainTextEdit.toPlainText().strip(),
+                                         int(self.spinBox.value()),
+                                         float(self.doubleSpinBox.value())))
+                self.parent.con.commit()
+                self.statusBar().showMessage('Информация успешно загружена')
+            else:
+                self.res = self.parent.cur.execute("""SELECT * FROM coffee_info WHERE id = (?)""",
+                                                   (int(self.parent.table.item(self.args[0], 0).text()),)).fetchall()
+                item = (self.lineEdit.text().strip(),
+                        self.comboBox.currentText().strip(),
+                        self.comboBox_2.currentText().strip(),
+                        self.plainTextEdit.toPlainText().strip(),
+                        int(self.spinBox.value()),
+                        float(self.doubleSpinBox.value()))
+                for i, info in enumerate(self.res[0][1:]):
+                    if info != item[i]:
+                        self.parent.cur.execute(
+                            f"""UPDATE coffee_info SET 
+                            {self.table_column[i]} = '{item[i]}' 
+                            WHERE id = {self.res[0][0]}""")
+                self.parent.con.commit()
+            self.parent.initUi()
+            QTimer.singleShot(600, lambda: self.close())
+
+    def check_confirm_form(self) -> bool:
+        if (self.lineEdit.text().strip() != '' and
+                self.spinBox.value() != 0 and
+                float(self.doubleSpinBox.value()) != 0.0):
+            return True
+        else:
+            self.statusBar().showMessage('Форма заполнена неверно')
+            QTimer.singleShot(5000, lambda: self.statusBar().showMessage(''))
+            return False
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Main()
+    ex = AddEditDatabase()
     ex.show()
     sys.exit(app.exec())
